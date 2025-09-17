865a5f11677ae139abe710f3ace79f5b289bca81
BroBordd <BroBordd@github.com>
2025-09-18 00:18:12 +0300

    yes

--- dist/ba_root/mods/ear.py
+++ dist/ba_root/mods/ear.py
@@ -9,14 +9,25 @@ For byBordd-server
 
 from babase import Plugin
 from _bascenev1 import (
+    get_foreground_host_activity as ga,
     broadcastmessage as push,
     chatmessage as say
 )
 
 def ear(m,i):
-    push(m)
-    say(f"lmfao {i} is funny")
+    if m.startswith('.'):
+        p = i2p(i)
+        push(f'{p}',clients=[i])
+        return
     return m
+
+def i2p(i):
+    a = ga()
+    k = None
+    for j,p in a.players:
+        if j.sessionplayer.inputdevice.client_id == i:
+            k = j
+    return k if k is None else a.players[k]
 
 # ba_meta require api 9
 # ba_meta export babase.Plugin
