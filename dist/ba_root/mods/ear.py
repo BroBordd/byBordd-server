6ce4c18c309262a5cbc221a10452dfc16cb4cdc9
BroBordd <BroBordd@github.com>
2025-09-17 18:47:50 +0300

    yes

---
+++ dist/ba_root/mods/ear.py
@@ -0,0 +1,23 @@
+# Copyright 2025 - Solely by BrotherBoard
+# Bug? Feedback? Telegram >> @BroBordd
+
+"""
+Ear v1.0 - Chat Listener
+
+For byBordd-server
+"""
+
+from babase import Plugin
+from bascenev1 import (
+    broadcastmessage as push,
+    chatmessage as say
+)
+
+def ear(m,i):
+    push(m)
+    say(f"lmfao {i} is funny")
+    return m
+
+# ba_meta require api 9
+# ba_meta export babase.Plugin
+class byBordd(Plugin): pass
